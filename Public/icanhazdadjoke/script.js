febb9d6a2a5acbca869e8700dca1305b65d24cdb
Paul McQuade <paulmcquad@gmail.com>
2020-05-21 19:07:24 +0100

    icanhazdadjoke API
    Search for dad jokes
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- Public/icanhazdadjoke/script.js
+++ Public/icanhazdadjoke/script.js
@@ -32,3 +32,44 @@ $(document).ready(function () {
     });
   });
 });
+
+$(document).ready(function () {
+  $("#searchjoke").click(function () {
+    var jokes = $("#jokes").val();
+
+    $.ajax({
+      url: "https://icanhazdadjoke.com/search?term=" + jokes,
+      dataType: "json",
+      type: "GET",
+      data: {},
+
+      success: function (data) {
+        var wf = "";
+        wf += "<p><b>" + data.results[0].id + "</p></b>" + data.results[0].joke;
+        $("#showsearchjoke").html(wf);
+        console.log(data);
+      },
+    });
+  });
+});
+
+/* $(document).ready(function () { */
+/*   $("#searchjokeid").click(function () { */
+/*     var jokes = $("#jokesid").val(); */
+/*  */
+/*     $.ajax({ */
+/*       url: "https://icanhazdadjoke.com/j/" + jokesid, */
+/*       dataType: "json", */
+/*       type: "GET", */
+/*       data: {}, */
+/*  */
+/*       success: function (data) { */
+/*         var wf = ""; */
+/*         wf += "<p><b>" + data.results[0].id + "</p></b>"; */
+/*         $("#searchjokeid").html(wf); */
+/*         console.log(data); */
+/*       }, */
+/*     }); */
+/*   }); */
+/* }); */
+/*  */

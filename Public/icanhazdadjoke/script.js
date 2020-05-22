6fc8288b8cad733fe9efa1857954f65739228b55
Paul McQuade <paulmcquad@gmail.com>
2020-05-22 17:20:47 +0100

    icanhazdadjoke API
    Added ID Joke function
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- Public/icanhazdadjoke/script.js
+++ Public/icanhazdadjoke/script.js
@@ -10,6 +10,8 @@ $.getJSON("https://icanhazdadjoke.com/", function (data) {
   /*   $(".joke").append(joke); */
 });
 
+// Get Joke
+
 $(document).ready(function () {
   $("#getjoke").click(function () {
     // var id = data.id;
@@ -33,6 +35,8 @@ $(document).ready(function () {
   });
 });
 
+// Search Joke
+
 $(document).ready(function () {
   $("#searchjoke").click(function () {
     var jokes = $("#jokes").val();
@@ -53,23 +57,28 @@ $(document).ready(function () {
   });
 });
 
-/* $(document).ready(function () { */
-/*   $("#searchjokeid").click(function () { */
-/*     var jokes = $("#jokesid").val(); */
-/*  */
-/*     $.ajax({ */
-/*       url: "https://icanhazdadjoke.com/j/" + jokesid, */
-/*       dataType: "json", */
-/*       type: "GET", */
-/*       data: {}, */
-/*  */
-/*       success: function (data) { */
-/*         var wf = ""; */
-/*         wf += "<p><b>" + data.results[0].id + "</p></b>"; */
-/*         $("#searchjokeid").html(wf); */
-/*         console.log(data); */
-/*       }, */
-/*     }); */
-/*   }); */
+//  ID Joke
+
+$(document).ready(function () {
+  $("#searchjokeid").click(function () {
+    var jokes = $("#jokesid").val();
+
+    $.getJSON("https://icanhazdadjoke.com/j/" + jokes, function (data) {
+      wf += "<p><b>" + data.id;
+
+      var wf = "";
+      wf += "<p><b>" + data.id + "</p></b>" + data.joke;
+      $("#pjoke").html(wf);
+
+      console.log(data.id);
+      console.log(data.joke);
+    });
+  });
+});
+
+// Debugging Info
+
+/* var jokesid = "R7UfaahVfFd"; */
+/* $.getJSON("https://icanhazdadjoke.com/j/" + jokesid, function (data) { */
+/*   console.log(data); */
 /* }); */
-/*  */
